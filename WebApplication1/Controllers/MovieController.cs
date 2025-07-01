95e250556f73ae8edf169d6631fe63ad204766cd
Furkan Heptaş <fleiddynn@gmail.com>
2025-07-01 15:30:29 +0300

    Add project files.

---
+++ WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+        [HttpPost]
+        public ActionResult<MovieInfo> AddMovie([FromBody] MovieInfo newMovie)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/MovieInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    public class MovieInfo
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public List<string> cast { get; set; }
+        public string director { get; set; }
+        public List<string> categories { get; set; }
+        public int imdb { get; set; }
+        public int length { get; set; }
+        public DateOnly releaseDate { get; set; }
+
+
+    }
+}
